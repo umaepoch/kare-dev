49177cc99d32c44c42505e0d0d505510a063bf73
umaepoch <umag@epochconsulting.in>
2021-12-09 14:09:08 +0530

    testing.

--- kare_dev/kare_dev/doctype/user_master/user_master.js
+++ kare_dev/kare_dev/doctype/user_master/user_master.js
@@ -1,8 +1,7 @@
 // Copyright (c) 2021, Pavithra M R and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('User Master', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on('User Master', "capture_image", function(frm, cdt, cdn) {
+	var doc = locals[cdt][cdn]
+	console.log(doc)
 });
