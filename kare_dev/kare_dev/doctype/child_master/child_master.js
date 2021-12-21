f965fc601272a9d10eb39e5be308b06385cad56a
NightsOfCoding <yashgowdaan@gmail.com>
2021-12-21 18:43:50 +0530

    api

--- kare_dev/kare_dev/doctype/child_master/child_master.js
+++ kare_dev/kare_dev/doctype/child_master/child_master.js
@@ -4,15 +4,17 @@ frappe.require("/assets/kare_dev/js/camera.js")
 
 frappe.ui.form.on("Images","activate_camera", function(frm, cdt, cdn){
   var doc = locals[cdt][cdn]
-  var cam = new Camera();
-
+  // var cam = new Camera();
+  var d = render()
+  d.show()
+  // console.log(MediaStreamHelper)
   // set options for cam based on deviceType.
   // returns device type (mobile/desktop).
-  cam.set_device_options()
+  // cam.set_device_options()
 
   // open camera.
-  cam.show()
-  cam.submit((data) => {
-    console.log(data)
-  })
+  // cam.show()
+  // cam.submit((data) => {
+  //   console.log(data)
+  // })
 });
