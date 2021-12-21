ceb3a17c73bb5b1c6d7ddd867e272380f277e311
NightsOfCoding <yashgowdaan@gmail.com>
2021-12-21 11:26:50 +0530

    Rear Camera test.

--- kare_dev/kare_dev/doctype/child_master/child_master.js
+++ kare_dev/kare_dev/doctype/child_master/child_master.js
@@ -1,9 +1,11 @@
 // Copyright (c) 2021, Pavithra M R and contributors
 // For license information, please see license.txt
+frappe.require("/assets/kare_dev/js/camera.js")
 
-frappe.ui.form.on('Child Master', {
-	images: function(frm) {
-		var doc = frm.doc
-		console.log(doc)
-	}
+frappe.ui.form.on("Images","activate_camera", function(frm, cdt, cdn){
+  var doc = locals[cdt][cdn]
+  console.log(doc)
+  // console.log(navigator.userAgent)
+  var cam = new Camera();
+  console.log(`device type : ${cam.set_device_options()}`)
 });
