7cdc3b3ba79d7cfc2a501062e5b9908056103484
NightsOfCoding <yashgowdaan@gmail.com>
2021-12-28 14:47:15 +0530

    preview test.

--- kare_dev/kare_dev/doctype/child_master/child_master.py
+++ kare_dev/kare_dev/doctype/child_master/child_master.py
@@ -12,20 +12,19 @@ class ChildMaster(Document):
 	pass
 
 @frappe.whitelist()
-def create_image_url():
+def create_image_url(doc):
 	try:
-		# img = json.loads(doc)
-		# b64 = base64.b64decode(img['data'])
-		# first_name = img['first_name']
-		# image_name = img['image_name']
-		# doctype = img['doctype']
-		# doc_name = img['doc_name']
+		img = json.loads(doc)
+		b64 = base64.b64decode(img['data'])
+		first_name = img['first_name']
+		image_name = img['image_name']
+		doctype = img['doctype']
+		doc_name = img['doc_name']
 
-		# print(img)
-		# if img:
-		# 	sf = save_file(first_name+"_"+image_name+".png", b64, doctype, doc_name)
-		# 	return {"SC": True, "file_url":sf.file_url}
+		if img:
+			sf = save_file(first_name+"_"+image_name+".png", b64, doctype, doc_name)
+			return {"SC": True, "file_url":sf.file_url}
 
-		return {"SC": "success"}
+		return {"SC": False}
 	except Exception as ex:
 		return {"EX": ex}
