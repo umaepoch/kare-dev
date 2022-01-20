243cab5d6a7c411570eeb9d9c00b8f4ffac96940
AkashMunikoti <akashmunikoti@gmail.com>
2022-01-20 17:16:15 +0530

    preliminary_fitment_report

--- kare_dev/kare_dev/doctype/preliminary_fitment_report/preliminary_fitment_report.js
+++ kare_dev/kare_dev/doctype/preliminary_fitment_report/preliminary_fitment_report.js
@@ -1,8 +1,149 @@
-// Copyright (c) 2021, Pavithra M R and contributors
+// Copyright (c) 2022, Pavithra M R and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Preliminary Fitment Report', {
-	// refresh: function(frm) {
-
-	// }
-});
+	refresh: function(frm, cdt, cdn) 
+		{
+		var d = locals[cdt][cdn];
+		var name=d.name;    
+		var case_proposal_no = fetch_caseproposal_name(name);    
+		cur_frm.set_value("case_proposal",case_proposal_no);
+		}
+	});
+	function fetch_caseproposal_name(name) {
+		var case_no = "";
+		frappe.call({
+			method: 'frappe.client.get_value',
+			args: {
+				'doctype': 'Case Proposal',
+				'fieldname': 'name',
+			},
+			async: false,
+			callback: function(r) {
+				if (r.message) {
+					// console.log(r.qty);
+					 case_no = r.message.name;
+				}
+			}
+		});
+		return  case_no;
+	}
+	//set value on selection of case proposal
+	frappe.ui.form.on('Preliminary Fitment Report', {
+	case_proposal: function(frm, cdt, cdn) {
+			var d = locals[cdt][cdn];
+			var case_proposal = d.case_proposal;
+			var full_name;
+			console.log("case_proposal",case_proposal);
+		if(case_proposal)
+			{	
+			var case_pro_details = fetch_proposal_deatails(case_proposal);
+			cur_frm.set_value("child",case_pro_details[0]['child'] || '');
+			full_name = (case_pro_details[0]['first_name'] || '')+ " " + (case_pro_details[0]['middle_name'] || '')+  (case_pro_details[0]['middle_name'] ? ' ':'') + (case_pro_details[0]['last_name'] || '');
+			cur_frm.set_value("child_name",full_name);
+			cur_frm.set_value("date_of_birth",case_pro_details[0]['date_of_birth'] || '');
+			cur_frm.set_value("gender",case_pro_details[0]['gender'] || '');
+			cur_frm.set_value("mothers_name",case_pro_details[0]['motherguardiancaregivers_name'] || '');
+			cur_frm.set_value("display_address",case_pro_details[0]['display_address'] || '');
+			
+			cur_frm.set_value("caregiver",case_pro_details[0]['caregiver'] || '');
+			cur_frm.set_value("caregiver_name",case_pro_details[0]['name_of_caregiver'] || '');		
+			cur_frm.set_value("education",case_pro_details[0]['education'] || '');
+			cur_frm.set_value("skill",case_pro_details[0]['skill'] || '');
+			cur_frm.set_value("employed",case_pro_details[0]['employed'] || '');
+			cur_frm.set_value("type_of_employment",case_pro_details[0]['type_of_employment'] || '');
+			
+			cur_frm.set_value("relationship_between_child_and_caregiver",case_pro_details[0]['how_is_caregiver_related_to_child'] || '');
+			
+			cur_frm.set_value("saathi",case_pro_details[0]['name_of_saathi'] || '');
+			
+			cur_frm.set_value("name_of_coordinator",case_pro_details[0]['name_of_coordinator'] || '');
+			cur_frm.set_value("case_type",case_pro_details[0]['case_type'] || '');
+				}
+				}
+			});
+			
+	function fetch_proposal_deatails(case_proposal)
+		{
+		  var case_d = "";
+		  frappe.call({
+			method: `kare_dev.kare_dev.doctype.preliminary_fitment_report.preliminary_fitment_report.get_case_proposal_details`,
+			args: {
+				 "name": case_proposal
+				  },
+				async: false,
+				callback: function(r) {
+				if (r.message) {
+				case_d = r.message;
+				}    
+				}
+				});
+				return  case_d;
+				}	
+			
+	frappe.ui.form.on('Preliminary Fitment Report', {
+	number_of_members_in_family: function(frm, cdt, cdn) 
+		{
+		var d = locals[cdt][cdn];
+		var income = d.total_income_of_family_per_month; 
+		var no_of_family = d.number_of_members_in_family;
+		console.log("income",income);
+		console.log("no_of_family",no_of_family);
+		
+		var month_income =  income/no_of_family;
+		if(!isNaN(month_income))
+		{
+		cur_frm.set_value("income_per_month_per_head",month_income);
+		}
+		 else
+		{
+		 cur_frm.set_value("income_per_month_per_head"," "); 
+		}
+	   
+	   
+		if(month_income <= 2000)
+		{
+		  cur_frm.set_value("income_eligibility","Yes");
+		}
+		else if(month_income >= 2000)
+		{
+		  cur_frm.set_value("income_eligibility","No"); 
+		}
+		else
+		{
+		 cur_frm.set_value("income_eligibility"," "); 
+		}
+		}
+	});
+	
+	frappe.ui.form.on('Preliminary Fitment Report', {
+	case_proposal: function(frm, cdt, cdn) 
+		{
+		var d = locals[cdt][cdn];
+		var date_of_birth=d.date_of_birth;    
+		var age = get_age(date_of_birth);   
+		console.log("age",age);
+		cur_frm.set_value("age",age[0]+" Years, "+age[1]+" Months");
+		}
+	});
+	function get_age(date_of_birth) {
+		var age = "";
+		frappe.call({
+			method: `kare_dev.kare_dev.doctype.preliminary_fitment_report.preliminary_fitment_report.calculateAge`,
+			args: {
+				 "date_of_birth": date_of_birth
+				  },
+				async: false,
+				callback: function(r) {
+				if (r.message) {
+				age = r.message;
+				}    
+				}
+				});
+				return  age;
+				}	
+	   
+	
+	
+	
+	
+	
--- kare_dev/kare_dev/doctype/preliminary_fitment_report/preliminary_fitment_report.py
+++ kare_dev/kare_dev/doctype/preliminary_fitment_report/preliminary_fitment_report.py
@@ -1,10 +1,50 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2021, Pavithra M R and contributors
+# Copyright (c) 2022, Pavithra M R and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
+from datetime import date
 
 class PreliminaryFitmentReport(Document):
 	pass
+
+@frappe.whitelist()
+def get_case_proposal_details(name):
+	print("name",name)	
+	get_case_data=frappe.db.sql("""select `tabCase Proposal`.`child`,`tabCase Proposal`.`display_address`,`tabCase Proposal`.`how_is_caregiver_related_to_child`,`tabCase Proposal`.`name_of_coordinator`,`tabCase Proposal`.`name_of_saathi`,`tabCase Proposal`.`case_type`,`tabCase Proposal`.`caregiver`,`tabCase Proposal`.`name_of_caregiver`,`tabCase Proposal`.`skill`,`tabCase Proposal`.`education`,`tabCase Proposal`.`employed`,`tabCase Proposal`.`type_of_employment`,`tabCase Proposal`.`name_of_saathi`,`tabCase Proposal`.`name_of_coordinator`,`tabCase Proposal`.`case_type`,`tabChild Master`.`motherguardiancaregivers_name`,`tabChild Master`.`date_of_birth`,`tabChild Master`.`first_name`,`tabChild Master`.`middle_name`,`tabChild Master`.`last_name`,`tabChild Master`.`gender` from `tabCase Proposal` join `tabChild Master` on `tabCase Proposal`.`child` = `tabChild Master`.`name` and `tabCase Proposal`.`name`='"""+name+"""' """, as_dict=1)
+	print("get_case_data",get_case_data)
+	return get_case_data
+
+@frappe.whitelist()
+def calculateAge(date_of_birth):
+	y = date.today().year
+	m = date.today().month
+	d = date.today().day
+	dob_convrt = datetime.strptime(date_of_birth, '%Y-%m-%d')
+	dob = dob_convrt.date()
+	print("dt",dob)
+	print (dob.year, dob.month, dob.day)
+
+	if dob.day > d and dob.month >= m:
+		dd = (d + 30) - dob.day
+		mm = ((m - 1) + 12) - dob.month
+		yy = (y - 1) - dob.year
+		return str(yy),str(mm),str(dd)
+	elif dob.day > d and dob.month < m:
+		dd = (d + 30) - dob.day
+		mm = m - dob.month
+		yy = y - dob.year
+		return str(yy),str(mm),str(dd)
+	elif dob.day < d and dob.month > m:
+		dd = d - dob.day
+		mm = (m + 12) - dob.month
+		yy = (y - 1) - dob.year
+		return str(yy),str(mm),str(dd)
+	else:
+		dd = d - dob.day
+		mm = m - dob.month
+		yy = y - dob.year
+		return str(yy),str(mm),str(dd)
