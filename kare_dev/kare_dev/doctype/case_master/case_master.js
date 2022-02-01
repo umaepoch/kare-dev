fdb51bbef8765a3a7be96950d2b4e529a43201d5
AkashMunikoti <akashmunikoti@gmail.com>
2022-02-01 17:58:01 +0530

    Case Master

--- kare_dev/kare_dev/doctype/case_master/case_master.js
+++ kare_dev/kare_dev/doctype/case_master/case_master.js
@@ -1,8 +1,80 @@
 // Copyright (c) 2021, Pavithra M R and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Case Master', {
-	// refresh: function(frm) {
-
-	// }
-});
+	case_proposal: function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var case_proposal = d.case_proposal;
+	var full_name;
+		console.log("case_proposal",case_proposal);
+		var case_proposal_data= fetch_case_data(case_proposal);
+		console.log("child_no00000000000",case_proposal_data);
+		for ( var i = 0; i < case_proposal_data.length; i++) {
+		
+		cur_frm.set_value("child",case_proposal_data[i]['child']);
+		cur_frm.set_value("name_of_child",case_proposal_data[i]['name_of_child']);
+		cur_frm.set_value("date_of_birth",case_proposal_data[i]['date_of_birth']);
+		cur_frm.set_value("address_of_child",case_proposal_data[i]['address']);
+		cur_frm.set_value("gender",case_proposal_data[i]['gender']);
+			cur_frm.set_value("image",case_proposal_data[i]['image']);
+		} 
+		}
+	});
+	function fetch_case_data(case_proposal)
+	{
+	
+	  console.log("function",case_proposal);
+	  console.log("entered into function");
+	  var case_d = "";
+	  frappe.call({
+		method: `kare_dev.kare_dev.doctype.preliminary_fitment_report.preliminary_fitment_report.get_case_details`,
+		args: {
+			 "case_proposal": case_proposal
+			  },
+			async: false,
+			callback: function(r) {
+			if (r.message) {
+			case_d = r.message;
+			console.log("case_d",case_d);
+			console.log("readings-----------" + JSON.stringify(r.message));
+				}    
+			}
+		});
+		return  case_d;
+	}
+	
+	frappe.ui.form.on('Case Master', {
+	caregiver: function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var caregiver = d.caregiver;
+	var full_name;
+		var case_caregiver_data= fetch_caregiver_data(caregiver);
+		console.log("child_no00000000000",case_caregiver_data);
+		for ( var i = 0; i < case_caregiver_data.length; i++) {
+		console.log("name",case_caregiver_data[i]['first_name']);
+		full_name = case_caregiver_data[i]['first_name'] + " " + case_caregiver_data[i]['middle_name'] + " " +case_caregiver_data[i]['last_name'];
+		cur_frm.set_value("name_of_caregiver",full_name);
+	
+	}    }
+	});
+	function fetch_caregiver_data(caregiver)
+	{
+	
+	  console.log("function",caregiver);
+	  console.log("entered into function");
+	  var case_d = "";
+	  frappe.call({
+		method: `kare_dev.kare_dev.doctype.case_master.case_master.get_caregiver_details`,
+		args: {
+			 "caregiver": caregiver
+			  },
+			async: false,
+			callback: function(r) {
+			if (r.message) {
+			case_d = r.message;
+			console.log("case_d",case_d);
+			console.log("readings-----------" + JSON.stringify(r.message));
+				}    
+			}
+		});
+		return  case_d;
+	}
--- kare_dev/kare_dev/doctype/case_master/case_master.py
+++ kare_dev/kare_dev/doctype/case_master/case_master.py
@@ -3,8 +3,22 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CaseMaster(Document):
 	pass
+
+@frappe.whitelist()
+def get_case_details(case_proposal):
+	print("child",case_proposal)	
+	get_case_data=frappe.db.sql("""select `tabChild Master`.`name`,`tabChild Master`.`name_of_child`,`tabChild Master`.`date_of_birth`,`tabChild Master`.`gender`,`tabChild Master`.`address`,`tabChild Master`.`image` from `tabChild Master` join `tabCase Proposal` on `tabCase Proposal`.`child` = `tabChild Master`.`name` and `tabCase Proposal`.`name`='"""+case_proposal+"""' """, as_dict=1)
+	print("pppppp",get_case_data)
+	return get_case_data
+
+@frappe.whitelist()
+def get_caregiver_details(caregiver):
+	print("child",caregiver)	
+	get_caregiver_data=frappe.db.sql("""select `tabChild Master`.`name`,`tabChild Master`.`name_of_child`,`tabChild Master`.`date_of_birth`,`tabChild Master`.`gender`,`tabChild Master`.`address`,`tabChild Master`.`image` from `tabChild Master` join `tabCase Proposal` on `tabCase Proposal`.`child` = `tabChild Master`.`name` and `tabCase Proposal`.`name`='"""+caregiver+"""' """, as_dict=1)
+	print("pppppp",get_caregiver_data)
+	return get_caregiver_data
