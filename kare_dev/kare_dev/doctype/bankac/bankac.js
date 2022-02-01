def72c3e61b538b5d53b58f0ca20853ec1f13fb9
AkashMunikoti <akashmunikoti@gmail.com>
2022-02-01 17:40:45 +0530

    Details Of Biological Parents

--- kare_dev/kare_dev/doctype/bankac/bankac.js
+++ kare_dev/kare_dev/doctype/bankac/bankac.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2021, Pavithra M R and contributors
 // For license information, please see license.txt
-
-frappe.ui.form.on('BankAC', {
-	// refresh: function(frm) {
-
-	// }
-});
+frappe.ui.form.on("BankAC", "account_holder_type", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var holder_type = frm.doc.account_holder_type;
+	console.log(holder_type);
+	
+	if(holder_type === "Caregiver")
+	{
+	   console.log(holder_type); 
+	   frm.set_query("account_holder_record", function() {
+	   
+			return {
+					filters: [
+						 ["Caregiver Master","name", "=", "account_holder_record"]
+						 
+					   ]
+					};
+				});
+		
+	}
+	if(holder_type === "Child")
+	{
+	   console.log(holder_type); 
+	   frm.set_query("account_holder_record", function() {
+	   
+			return {
+					filters: [
+						 ["Child Master","name", "=", "account_holder_record"]
+						 
+					   ]
+					};
+				});
+	}
+	});
+	
+	
