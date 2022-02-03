bd76269d0450779bd355080486aa53a20af86823
AkashMunikoti <akashmunikoti@gmail.com>
2022-02-03 18:09:47 +0530

    Baseline Questions Child

---
+++ kare_dev/kare_dev/doctype/baseline_questions_child/baseline_questions_child.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Pavithra M R and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Baseline Questions Child', {
+	// refresh: function(frm) {
+
+	// }
+});
