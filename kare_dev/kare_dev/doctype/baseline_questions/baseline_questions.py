18d89ed6468a4f10c3d66d9a78260dbbc356c313
AkashMunikoti <akashmunikoti@gmail.com>
2022-02-25 15:32:20 +0530

    baseline Questions Edited

--- kare_dev/kare_dev/doctype/baseline_questions/baseline_questions.py
+++ kare_dev/kare_dev/doctype/baseline_questions/baseline_questions.py
@@ -11,6 +11,12 @@ class BaselineQuestions(Document):
 
 @frappe.whitelist()
 def get_questions():
-	get_questions_details=frappe.db.sql("""select questions,area,5_to_9,10_to_12,13_to_15,16_to_18 from `tabQuestions`""", as_dict=1)
+	get_questions_details=frappe.db.sql("""select name,questions,area,5_to_9,10_to_12,13_to_15,16_to_18 from `tabQuestions`""", as_dict=1)
 	print("get_questions_details",get_questions_details)
 	return get_questions_details
+
+@frappe.whitelist()
+def select_action(parent,options):
+	get_action_details=frappe.db.sql("""select `tabActions`.`options`,`tabActions`.`actions_need_to_be_taken`,`tabActions`.`parent` from `tabActions` where parent='"""+parent+"""' and options='"""+options+"""' """, as_dict=1)
+	print("actions",get_action_details)
+	return get_action_details
