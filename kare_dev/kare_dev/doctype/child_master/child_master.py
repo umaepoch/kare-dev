1d6b8a13eb6167c419619a688f219e258e12c023
AkashMunikoti <akashmunikoti@gmail.com>
2022-09-02 18:51:43 +0530

    changes in child master

--- kare_dev/kare_dev/doctype/child_master/child_master.py
+++ kare_dev/kare_dev/doctype/child_master/child_master.py
@@ -7,6 +7,8 @@ from frappe.utils.file_manager import save_file
 from frappe.contacts.address_and_contact import load_address_and_contact, delete_contact_and_address
 import base64
 import json
+from datetime import datetime
+from datetime import date
 
 class ChildMaster(Document):
 	def onload(self):
@@ -71,7 +73,7 @@ def check_child(adhar):
 	return check_child_record
 	
 @frappe.whitelist()
-def update_details(name,gender,first_name,middle_name,last_name,date_of_birth,caregiver_address):
+def update_details_case_prop(name,gender,first_name,middle_name,last_name,date_of_birth,caregiver_address):
 	print("datas",name,gender)
 	case_name =frappe.db.sql("""select name from `tabCase Proposal` where child ='"""+name+"""' """, as_dict=1)
 	if case_name != [ ]:
@@ -79,13 +81,17 @@ def update_details(name,gender,first_name,middle_name,last_name,date_of_birth,ca
 			print("inside loop",case_name)
 			test_doc = frappe.get_doc("Case Proposal",i['name'])
 			if gender != [ ]: 
-				test_doc.gender = gender
+				#test_doc.gender = gender
+				frappe.db.set_value('Case Proposal',i['name'],"gender",gender)
 			if first_name != [ ] and middle_name != [ ] and last_name != [ ]:  
-				test_doc.name_of_child = first_name+" "+middle_name+" "+last_name
+				frappe.db.set_value('Case Proposal',i['name'],"name_of_child",first_name+" "+middle_name+" "+last_name)
+				#test_doc.name_of_child = first_name+" "+middle_name+" "+last_name
 			if date_of_birth != [ ]: 
-				test_doc.date_of_birth = date_of_birth
+				#test_doc.date_of_birth = date_of_birth
+				frappe.db.set_value('Case Proposal',i['name'],"date_of_birth",date_of_birth)
 			if caregiver_address != [ ]: 
-				test_doc.display_address = caregiver_address
+				#test_doc.display_address = caregiver_address
+				frappe.db.set_value('Case Proposal',i['name'],"display_address",caregiver_address)
 			test_doc.save()
 			return case_name
 	
@@ -122,16 +128,17 @@ def update_child_details_ass(name,gender,first_name,middle_name,last_name,date_o
 				test_d.child_name = first_name+" "+middle_name+" "+last_name
 			if date_of_birth != [ ]: 
 				test_d.date_of_birth = date_of_birth
+			
 			test_d.save()
 			return ass
 
 @frappe.whitelist()
 def update_child_details_case_m(name,gender,first_name,middle_name,last_name,date_of_birth,caregiver_address):
 	print("datas",name,gender)
-	ass =frappe.db.sql("""select name from `tabCase Master` where child ='"""+name+"""' """, as_dict=1)
-	if ass != [ ]:
-		for k in ass:
-			print("inside loop",ass)
+	ca =frappe.db.sql("""select name from `tabCase Master` where child ='"""+name+"""' """, as_dict=1)
+	if ca != [ ]:
+		for k in ca:
+			print("inside loop",ca)
 			test_d = frappe.get_doc("Case Master",k['name'])
 			if gender != [ ]: 
 				test_d.gender = gender
@@ -142,4 +149,104 @@ def update_child_details_case_m(name,gender,first_name,middle_name,last_name,dat
 			if caregiver_address != [ ]: 
 				test_d.address_of_child = caregiver_address
 			test_d.save()
-			return ass
+			return ca
+
+@frappe.whitelist()
+def update_dob_preli(name,date_of_birth):
+	print("dob",name,date_of_birth)
+	prelim =frappe.db.sql("""select name from `tabPreliminary Fitment Report` where child ='"""+name+"""' """, as_dict=1)
+	if prelim != [ ]:
+		for s in prelim:
+			print("inside loop preli",s)
+			test_dob1 = frappe.get_doc("Preliminary Fitment Report",s['name'])
+			if date_of_birth != [ ]: 
+				y = date.today().year
+				m = date.today().month
+				d = date.today().day
+				dob_convrt = datetime.strptime(date_of_birth, '%Y-%m-%d')
+				dob = dob_convrt.date()
+				print("dt preli",dob)
+				print (dob.year, dob.month, dob.day)
+
+				if dob.day > d and dob.month >= m:
+					print("inside if preli")
+					dd = (d + 30) - dob.day
+					mm = ((m - 1) + 12) - dob.month
+					yy = (y - 1) - dob.year
+					frappe.db.set_value('Preliminary Fitment Report',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 1",test_dob1.age)
+					return str(yy),str(mm),str(dd)
+				elif dob.day > d and dob.month < m:
+					dd = (d + 30) - dob.day
+					mm = m - dob.month
+					yy = y - dob.year
+					frappe.db.set_value('Preliminary Fitment Report',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 2",test_dob1.age)
+					return str(yy),str(mm),str(dd)
+				elif dob.day < d and dob.month > m:
+					print("inside else if 2")	
+					dd = d - dob.day
+					mm = (m + 12) - dob.month
+					yy = (y - 1) - dob.year
+					frappe.db.set_value('Preliminary Fitment Report',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 3",test_dob1.age)
+					return str(yy),str(mm),str(dd)
+				else:	
+					print("inside else")	
+					dd = d - dob.day
+					mm = m - dob.month
+					yy = y - dob.year
+					frappe.db.set_value('Preliminary Fitment Report',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 4",test_dob1.age)
+					return str(yy),str(mm),str(dd)
+			test_dob1.save()
+			
+@frappe.whitelist()
+def update_dob_assess(name,date_of_birth):
+	print("dob",name,date_of_birth)
+	preli =frappe.db.sql("""select name from `tabAssessment Intake Form` where child ='"""+name+"""' """, as_dict=1)
+	if preli != [ ]:
+		for s in preli:
+			print("inside loop assess",s)
+			test_dob = frappe.get_doc("Assessment Intake Form",s['name'])
+			if date_of_birth != [ ]: 
+				y = date.today().year
+				m = date.today().month
+				d = date.today().day
+				dob_convrt = datetime.strptime(date_of_birth, '%Y-%m-%d')
+				dob = dob_convrt.date()
+				print("dt",dob)
+				print (dob.year, dob.month, dob.day)
+
+				if dob.day > d and dob.month >= m:
+					print("inside if assess")
+					dd = (d + 30) - dob.day
+					mm = ((m - 1) + 12) - dob.month
+					yy = (y - 1) - dob.year
+					frappe.db.set_value('Assessment Intake Form',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 1",test_dob.age)
+					return str(yy),str(mm),str(dd)
+				elif dob.day > d and dob.month < m:
+					dd = (d + 30) - dob.day
+					mm = m - dob.month
+					yy = y - dob.year
+					frappe.db.set_value('Assessment Intake Form',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 2",test_dob.age)
+					return str(yy),str(mm),str(dd)
+				elif dob.day < d and dob.month > m:
+					print("inside else if 2")	
+					dd = d - dob.day
+					mm = (m + 12) - dob.month
+					yy = (y - 1) - dob.year
+					frappe.db.set_value('Assessment Intake Form',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 3",test_dob.age)
+					return str(yy),str(mm),str(dd)
+				else:	
+					print("inside else")	
+					dd = d - dob.day
+					mm = m - dob.month
+					yy = y - dob.year
+					frappe.db.set_value('Assessment Intake Form',s['name'],"age",str(yy)+" Years "+str(mm)+" Months")
+					print("test_dob.age 4",test_dob.age)
+					return str(yy),str(mm),str(dd)
+			test_dob.save()
